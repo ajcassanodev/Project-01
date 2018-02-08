f32db7a60c49394b038751676a1ec291345bc9c8
ajcassanodev <ajcassanodev@users.noreply.github.com>
2018-02-08 09:30:54 -0500

    4 of 5 quotes entered

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+var quotes {
+    [quote: 'you miss 100 perscent of the shotes you dont take.', source: 'Wayne Gretzky'],
+    [quote: 'To be the man, you must beat the man.', source: 'Ric Flair'],
+    [quote: 'Without self-discipline, success is impossible, period.', source: 'Lou Holtz'],
+    [quote: 'It is not the size of a man but the size of his heart that matters', source: 'Evander Holyfield'],
+    [quote: '', source: '']
+};
+
+
+
+
+
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
