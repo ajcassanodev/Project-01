a1e509c9234c38649327bf7c2e45790942bddf46
ajcassanodev <ajcassanodev@users.noreply.github.com>
2018-02-09 14:02:31 -0500

    printquote function
    fixed printquote function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 var quotes = [
-    {quote: 'you miss 100 perscent of the shotes you dont take.', source: 'Wayne Gretzky'},
+    {quote: 'you miss 100 perscent of the shots you dont take.', source: 'Wayne Gretzky'},
     {quote: 'To be the man, you must beat the man.', source: 'Ric Flair'},
     {quote: 'Without self-discipline, success is impossible, period.', source: 'Lou Holtz'},
     {quote: 'It is not the size of a man but the size of his heart that matters', source: 'Evander Holyfield'},
     {quote: 'Winners never quit and quitters never win.', source: 'Vince Lombardi'}
 ];
-
+//this selects a random quote and source based on length of array; it returns the value back. 
 function getRandomQuote () {
     var randomQuote = Math.floor( Math.random() * quotes.length);
-    return randomQuote; 
+    return quotes[randomQuote]; 
 }
-
+//this function calls getRandomQuote function  of getRandomQuote function and adds it into the index.html 
 function printQuote () {
-    var printq = getRandomQuote(quotes.quote);
-    return printq;
-
+    var printq = getRandomQuote() ;
+ var quoteb  = '<p class="quote">' + printq.quote + '</p>' + '<p class="source">' + printq.source + '</p>';  
+    document.getElementById('quote-box').innerHTML = quoteb ;
 }
 
 
