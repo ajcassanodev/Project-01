c9e45f4bd322c96fa5983bae0dcb5ecc7ff949f6
ajcassanodev <ajcassanodev@users.noreply.github.com>
2018-02-09 15:13:17 -0500

    final
    program complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+//array of objects 
 var quotes = [
-    {quote: 'you miss 100 perscent of the shots you dont take.', source: 'Wayne Gretzky'},
+    {quote: 'you miss 100 percent of the shots you dont take.', source: 'Wayne Gretzky'},
     {quote: 'To be the man, you must beat the man.', source: 'Ric Flair'},
     {quote: 'Without self-discipline, success is impossible, period.', source: 'Lou Holtz'},
     {quote: 'It is not the size of a man but the size of his heart that matters', source: 'Evander Holyfield'},
@@ -10,11 +11,12 @@ function getRandomQuote () {
     var randomQuote = Math.floor( Math.random() * quotes.length);
     return quotes[randomQuote]; 
 }
-//this function calls getRandomQuote function  of getRandomQuote function and adds it into the index.html 
+//this function calls getRandomQuote function  of getRandomQuote; the values are stored into the quoteb var.   
 function printQuote () {
-    var printq = getRandomQuote() ;
- var quoteb  = '<p class="quote">' + printq.quote + '</p>' + '<p class="source">' + printq.source + '</p>';  
+    var printq  = getRandomQuote() ;
+    var quoteb  = '<p class="quote">' + printq.quote + '</p>' + '<p class="source">' + printq.source + '</p>';  
     document.getElementById('quote-box').innerHTML = quoteb ;
+    
 }
 
 
